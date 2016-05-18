8d3a3d8cfb631208d9d6b4070232d727889939c6
johnduro <martin.leroy6@gmail.com>
2016-05-18 18:21:17 +0200

    debut du match des routes

--- app/RequestHandler.php
+++ app/RequestHandler.php
@@ -11,9 +11,48 @@ class RequestHandler
         $this->url = $url;
     }
 
+    private function findRouteFromUrl()
+    {
+        $ret = array(
+            'match' => false,
+            'route' => null,
+        );
+
+        echo '<pre>';
+        foreach ($this->routes as $name => $route)
+        {
+            echo "ROUTE - " . $name . " - :<br/>"; //TODELETE
+            echo "PATH :: " . $route["path"] . "<br/>";//TODELTE
+
+            //get args -- \/(\{[^\}]+\})\/? -- DONE
+            //replace args with pattern (.*)
+            //transform all path in pattern
+            //compare url with pattern
+            //if match return true
+
+            $matches = array();
+
+            if (($nbMatch = preg_match_all("/\/(\{[^\}]+\})\/?/", $route['path'], $matches)) > 0)
+            {
+                echo 'MATCHED ::<br/>';
+                var_dump($matches);
+                echo '<br/>';
+
+            }
+        }
+        echo '</pre>';
+
+        return ($ret);
+    }
+
     public function treatRequest()
     {
         echo 'TREAT REQUEST : <br/>';//TODELETE
         var_dump($this->routes);//TODELETE
+        echo '<br/>';//TODELETE
+
+        $routeSearch = $this->findRouteFromUrl();
+
+        var_dump($routeSearch);//TODELETE
     }
 }
--- config/config.php
+++ config/config.php
@@ -3,5 +3,8 @@
 /**
  * Parse routing.json
  */
+
+//CHECK JSON AND ROUTING VALIDITY !!
+//check if path isset else throw error
 $content = file_get_contents(ROOT . DS . 'config' . DS . 'routing.json');
-$APP_ROUTES = json_decode($content);
+$APP_ROUTES = json_decode($content, true);
